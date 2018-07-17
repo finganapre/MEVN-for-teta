1780553f8de1428ea603257aa05e57b190a90cb4
finganapre <finganapre@gmail.com>
2018-07-17 18:57:07 +0500

    start base template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
+    <MainHeader></MainHeader>
     <router-view/>
+    <MainFooter></MainFooter>
   </div>
 </template>
 
 <script>
+import MainHeader from './components/stdElem/MainHeader.vue'
+import MainFooter from './components/stdElem/MainFooter.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+  	MainHeader: MainHeader,
+  	MainFooter: MainFooter
+  }
 }
 </script>
 
---
+++ client/src/components/stdElem/MainFooter.vue
@@ -0,0 +1,15 @@
+<template>
+	<footer class="MainFooter">
+		подвал
+	</footer>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style scoped>
+	
+</style>
